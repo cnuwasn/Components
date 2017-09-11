87c9f5f8889c26ef786d6a519836ba5e9eb17e8e
Srinivasan Natarajan <srinivasan.natarajan@tavant.com>
2017-09-11 14:16:51 +0530

    animation has been added

--- Javascript/Pie-chart/pie.js
+++ Javascript/Pie-chart/pie.js
@@ -1,43 +1,58 @@
 document.pie = function (id, obj) {
     var pie = document.getElementById(id);
-    var centerX = pie.width / 2;
-    var centerY = pie.height / 2;
+    var centerX, centerY, egendPosition;
+	if (pie.width > pie.height) {
+		centerX = pie.height / 2;
+		centerY = centerX;
+		legendPosition = "right";
+	} else {
+		centerX = pie.width / 2;
+		centerY = centerX;
+		legendPosition = "bottom";
+	}
 	var inputradius = obj.radius;
     var outercircleRadius = (((centerX > centerY) ? centerY : centerX) > inputradius) ? inputradius : ((centerX > centerY) ? centerY : centerX);
     var contextEl = pie.getContext("2d");
 	var tickColor, sliceAngle;
 	var startAngle = obj.startAngle;
 	var endAngle = degreesToRadians(startAngle);
-	var animationRadiusVariation = outercircleRadius / 20;
+	var animationRadiusVariation = outercircleRadius/ 10;
 	var startAnimation, drawRadius = 0;
+	var sweep = obj.sweepAngle;
 	var circleObj = {
 		startAngle: startAngle,
 		endAngle: endAngle
 	};
 	if (obj.enableAnimation) {
 		drawRadius = animationRadiusVariation;
-		startAnimation = setInterval(drawPieChart, 50);
+		sweep = 36;
+		startAnimation = setInterval(drawPieChart, 25);
 	}
 	else {
 		drawRadius = outercircleRadius;
 		drawPieChart();
 	}
 	function drawPieChart() {
-		if (obj.enableAnimation && drawRadius >= outercircleRadius) {
+		circleObj.endAngle = degreesToRadians(obj.startAngle);
+		contextEl.clearRect(0, 0, pie.width, pie.height);
+		if (obj.enableAnimation && sweep >= obj.sweepAngle && drawRadius >= outercircleRadius ) {
 			drawRadius = outercircleRadius;
 			clearInterval(startAnimation);
+			if (obj.dropShadow)
+				drawShadow();
 		}
-		contextEl.clearRect(0, 0, pie.width, pie.height);
 		for (var i = 0; i < obj.ranges.length; i++) {
 			circleObj.tickColor = obj.ranges[i].color;
-			sliceAngle = obj.sweepAngle / 100 * obj.ranges[i].size;
+			sliceAngle = sweep / 100 * obj.ranges[i].size;
 			circleObj.startAngle = circleObj.endAngle;
 			circleObj.endAngle = circleObj.endAngle + degreesToRadians(sliceAngle);
 			circleObj.radius = drawRadius;
 			drawPie(contextEl, circleObj);
 		}
-		if (obj.enableAnimation)
+		if (obj.enableAnimation) {
 			drawRadius += animationRadiusVariation;
+			sweep += 36;
+		}
 	}
 	function degreesToRadians(degrees) {
         return (Math.PI / 180) * degrees
@@ -50,4 +65,20 @@ document.pie = function (id, obj) {
 		ctx.closePath();
 		ctx.fill();
     }
+	function drawShadow() {
+		var x = centerX / 15,
+			y = centerY + outercircleRadius,
+			innerRadius = 5,
+			radius = outercircleRadius / 20;
+		contextEl.scale(15, 1);
+		var gradient = contextEl.createRadialGradient(x, y, innerRadius, x, y, outercircleRadius);
+		gradient.addColorStop(0, '#f6f6f6');
+		gradient.addColorStop(0.1, 'white');
+		gradient.addColorStop(1, '#8c8c8c');
+
+		contextEl.arc(x, y, radius, 0, 2 * Math.PI);
+		contextEl.fillStyle = gradient;
+		contextEl.fill();
+		contextEl.scale(1 / 15, 1);
+	}
 }
